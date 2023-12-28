a5daddcc9fa0bcb669b4b3ade25a2dc70a38679c
Anubhav <anubhavanand848@gmail.com>
2023-12-28 16:04:23 +0530

    Added my project

---
+++ app/api/ping/route.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from "next/server";
+
+function ping(){
+    return NextResponse.json({
+        messge:"hello Next JS"
+    })
+}
+
+async function pingwith(req:NextRequest){
+    const body = await req.json();
+    const {email,password}= body;
+
+    return NextResponse.json({
+        email,
+        password,
+        messge:"Another API request",
+    })
+}
+
+export {ping as GET, pingwith as POST} 
